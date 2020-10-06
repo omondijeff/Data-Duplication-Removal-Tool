f4af5858911e846859c0b43adeecda2f1f45dad9
omondijeff <11554jeff@gmail.com>
2020-10-06 16:25:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import os
 import hashlib
 from collections import defaultdict
 import csv
+
+"Given a folder, this tool walks through it and creates a list of all duplicate files"
+"the md5 checksum for each file determines duplicates"
+
+src_folder = "../../"
